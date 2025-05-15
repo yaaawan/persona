81902ae33ec016dc747f4c8baef329b082376156
suci <sucisucisriaulia@gmail.com>
2025-05-15 21:10:13 +0700

    HadiahHP1

--- user/hadiah/HadiahHP.java
+++ user/hadiah/HadiahHP.java
@@ -1,1 +1,14 @@
+package user.hadiah;
 
+import user.User;
+
+public class HadiahHP extends Hadiah {
+    private int tambahanHP = 10; 
+    public HadiahHP() {
+        super("Hadiah HP (10)");
+    }
+    
+    public HadiahHP(int jumlahHP) {
+        super("Hadiah HP (" + jumlahHP + ")");
+        this.tambahanHP = jumlahHP;
+    }
