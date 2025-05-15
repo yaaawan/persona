c5bde974116447c616e5568ee4cf0efb335fc973
diestymendila <diestymendila@gmail.com>
2025-05-16 06:26:28 +0800

    Skill

--- user/skill/Skill.java
+++ user/skill/Skill.java
@@ -1,1 +1,13 @@
+package user.skill;
 
+public abstract class Skill implements UsableSkill {
+    protected String namaSkill;
+
+    public Skill(String namaSkill) {
+        this.namaSkill = namaSkill;
+    }
+
+    public String getNamaSkill() {
+        return namaSkill;
+    }
+}
