a41103e1d2eb2e44b4cd2a3a24dae9279047d554
MONTANA <yudbryawan@BRYAWANMONTANA.local>
2025-05-16 00:43:24 +0800

    Main_1

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+import pertarungan.Dungeon;
+import user.User;
 
+public class Main {
+    private static final String HORIZONTAL_LINE = "==================================================================";
+    private static final String DOUBLE_LINE = "======================================================================";
+    private static final String DOTTED_LINE = "----------------------------------------------------------------------";
+    private static final String WAVE_LINE = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_BOLD = "\u001B[1m";
