9101392d885fe780b19376a00f194f350e35cd41
diestymendila <diestymendila@gmail.com>
2025-05-15 20:11:41 +0800

    Skill Air

--- user/skill/Air.java
+++ user/skill/Air.java
@@ -1,1 +1,24 @@
+package user.skill;
 
+public class Air extends Skill {
+    public Air() {
+        super("Air");
+    }
+
+    @Override
+    public void gunakanSkill() {
+        System.out.println("Menggunakan skill air!");
+    }
+
+    public void gunakanSkill(String target) {
+        System.out.println("Menggunakan skill air untuk membasahi " + target + "!");
+    }
+
+    public void gunakanSkill(int volume) {
+        System.out.println("Menggunakan skill air dengan volume " + volume + " liter!");
+    }
+
+    public void gunakanSkill(String target, int volume) {
+        System.out.println("Menggunakan skill air dengan volume " + volume + " liter untuk membasahi " + target + "!");
+    }
+}
