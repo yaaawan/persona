1075d23db4afcabc42c8913928c55b83a1e6666d
diestymendila <diestymendila@gmail.com>
2025-05-16 04:21:58 +0800

    Skill Api 2

--- user/skill/Api.java
+++ user/skill/Api.java
@@ -9,3 +9,16 @@ public class Api extends Skill {
     public void gunakanSkill() {
         System.out.println("Menggunakan skill api!");
     }
+
+    public void gunakanSkill(String target) {
+        System.out.println("Menggunakan skill api untuk membakar " + target + "!");
+    }
+
+    public void gunakanSkill(int kekuatan) {
+        System.out.println("Menggunakan skill api dengan kekuatan " + kekuatan + "!");
+    }
+
+    public void gunakanSkill(String target, int kekuatan) {
+        System.out.println("Menggunakan skill api dengan kekuatan " + kekuatan + " untuk membakar " + target + "!");
+    }
+}
