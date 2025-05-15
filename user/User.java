13947ea36f7b5edde1326d6b38ca8409b3031bea
MONTANA <yudbryawan@BRYAWANMONTANA.local>
2025-05-15 20:54:11 +0800

    User_2

--- user/User.java
+++ user/User.java
@@ -21,3 +21,45 @@ public class User {
         this.totalHadiahSP = 0; 
     }
 
+     public String getNama() {
+        return nama;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public String getSkill() {
+        return skill.getNamaSkill().toLowerCase();
+    }
+
+    public void tambahHP(int jumlah) {
+        persona.tambahHp(jumlah);
+    }
+
+    public void tambahSP(int jumlah) {
+        persona.tambahSp(jumlah);
+
+        totalHadiahSP += jumlah;
+    }
+
+    public int getTotalHadiahSP() {
+        return totalHadiahSP;
+    }
+
+    public void tambahRiwayatHadiah(Hadiah hadiah) {
+        riwayatHadiah.add(hadiah);
+    }
+
+    public void tampilkanRiwayatHadiah() {
+        if (riwayatHadiah.isEmpty()) {
+            System.out.println("Belum ada hadiah yang Anda dapatkan.");
+            return;
+        }
+        
+        int count = 1;
+        for (Hadiah hadiah : riwayatHadiah) {
+            System.out.println(count++ + ". " + hadiah.getNamaHadiah());
+        }
+    }
+
