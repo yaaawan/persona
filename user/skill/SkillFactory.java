3667e7fbaa19582ae81b4ea9ec239fd42058934e
diestymendila <diestymendila@gmail.com>
2025-05-16 07:54:37 +0800

    Skill Factory 1

--- user/skill/SkillFactory.java
+++ user/skill/SkillFactory.java
@@ -1,1 +1,9 @@
+package user.skill;
 
+public class SkillFactory {
+    public static Skill getSkill(String skillInput) {
+        switch (skillInput.toLowerCase()) {
+            case "api":
+                return new Api();
+            case "air":
+                return new Air();
