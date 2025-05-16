3063d86616c36663e5af4afa53c6d84810f64a91
diestymendila <diestymendila@gmail.com>
2025-05-16 09:34:01 +0800

    Skill Tanah 1

--- user/skill/Tanah.java
+++ user/skill/Tanah.java
@@ -1,1 +1,11 @@
+package user.skill;
 
+public class Tanah extends Skill {
+    public Tanah() {
+        super("Tanah");
+    }
+
+    @Override
+    public void gunakanSkill() {
+        System.out.println("Menggunakan skill tanah!");
+    }
