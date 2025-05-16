6d8236a10244f520d9f2547fb989d5d1c1a3b405
L-cmd19 <levianvanhossen@gmail.com>
2025-05-16 09:45:08 +0800

    Musuh1

--- pertarungan/Musuh.java
+++ pertarungan/Musuh.java
@@ -1,1 +1,14 @@
+package pertarungan;
 
+public class Musuh {
+    private String nama;
+    private String skill;
+    private int hp;
+    private int sp;
+
+    public Musuh(String nama, String skill) {
+        this.nama = nama;
+        this.skill = skill.toLowerCase();
+        this.hp = 80;
+        this.sp = 40;
+    }
