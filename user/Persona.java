cde17ce372da06be0cce356626bfc4e3345b6e8d
diestymendila <diestymendila@gmail.com>
2025-05-16 19:03:54 +0800

    Persona 2

--- user/Persona.java
+++ user/Persona.java
@@ -12,3 +12,42 @@ public class Persona {
         this.hp = 100;
         this.sp = 50;
     }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getSp() {
+        return sp;
+    }
+
+    public void tambahHp(int jumlah) {
+        this.hp += jumlah;
+    }
+
+    public void tambahSp(int jumlah) {
+        this.sp += jumlah;
+    }
+
+    public void kurangHp(int jumlah) {
+        this.hp = Math.max(0, this.hp - jumlah);
+    }
+
+    public void kurangSp(int jumlah) {
+        this.sp = Math.max(0, this.sp - jumlah);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Persona: %s | Skill: %s | HP: %d | SP: %d",
+            nama, skill.toUpperCase(), hp, sp);
+    }
+}
