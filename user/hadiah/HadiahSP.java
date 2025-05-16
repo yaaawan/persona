2474e3a030a2ef16aa6e5470ceda6fd4cd7c6fab
suci <sucisucisriaulia@gmail.com>
2025-05-16 10:18:16 +0700

    HadiahSP1

--- user/hadiah/HadiahSP.java
+++ user/hadiah/HadiahSP.java
@@ -1,1 +1,14 @@
+package user.hadiah;
 
+import user.User;
+
+public class HadiahSP extends Hadiah {
+    private int tambahanSP = 5;
+    public HadiahSP() {
+        super("Hadiah SP (5)");
+    }
+    
+    public HadiahSP(int jumlahSP) {
+        super("Hadiah SP (" + jumlahSP + ")");
+        this.tambahanSP = jumlahSP;
+    }
