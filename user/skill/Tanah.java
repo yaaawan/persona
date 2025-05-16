01834cf7fbdcf8a471b12adf0d1992dfac53f180
diestymendila <diestymendila@gmail.com>
2025-05-16 11:43:46 +0800

    Skill Tanah 2

--- user/skill/Tanah.java
+++ user/skill/Tanah.java
@@ -9,3 +9,16 @@ public class Tanah extends Skill {
     public void gunakanSkill() {
         System.out.println("Menggunakan skill tanah!");
     }
+
+    public void gunakanSkill(String target) {
+        System.out.println("Menggunakan skill tanah untuk menimbun " + target + "!");
+    }
+
+    public void gunakanSkill(int massa) {
+        System.out.println("Menggunakan skill tanah dengan massa " + massa + " kg!");
+    }
+
+    public void gunakanSkill(String target, int massa) {
+        System.out.println("Menggunakan skill tanah dengan massa " + massa + " kg untuk menimbun " + target + "!");
+    }
+}
