daa644e5ae8e51f1bdfdad3ab64c4ce92c0c34c9
diestymendila <diestymendila@gmail.com>
2025-05-16 17:53:40 +0800

    Persona 1

--- user/Persona.java
+++ user/Persona.java
@@ -1,1 +1,14 @@
+package user;
 
+public class Persona {
+    private String nama;
+    private String skill;
+    private int hp;
+    private int sp;
+
+    public Persona(String nama, String skill) {
+        this.nama = nama;
+        this.skill = skill.toLowerCase();
+        this.hp = 100;
+        this.sp = 50;
+    }
